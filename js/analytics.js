b1779b8f85ed117f5d24b8b665bdb6668c1c55f4
Yerson-Martinez97 <yersonmartinezdev@gmail.com>
2025-06-01 19:46:03 -0400

    analytic

--- js/analytics.js
+++ js/analytics.js
@@ -1,7 +1,5 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-gtag("config", "G-60NM5HJRQ1");
+  gtag('config', 'G-81J6B2SQZ5');
